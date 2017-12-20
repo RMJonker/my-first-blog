b1a864f038126b3643c18017d28a7d9df92f4fc9
s165318 <r.m.jonker@student.tue.nl>
2017-12-20 15:11:37 +0100

    Should be the working website including js and css

--- A2/urls.py
+++ A2/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.index, name='index'),
     url(r'^$', views.help, name='help'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
--- A2/views.py
+++ A2/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -7,5 +9,6 @@ def index(request):
 def help(request):
 	return render(request, 'A2/help.html')
 
-"""def post_list(request):
-    return render(request, 'A2/post_list.html')"""
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'A2/post_list.html', {})
